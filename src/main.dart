6ea6ed087ca082d1d5310694cf5d7043c22939e7
Patrik Jansson <patrik.ja@gmail.com>
2015-06-04 11:14:12 +0200

    Socket connect

--- src/main.dart
+++ src/main.dart
@@ -1,3 +1,12 @@
-main() {
-  print("Hello world");
+import 'dart:io';
+
+void main() {
+  var future = Socket.connect("localhost", 6667);
+  // Now register a callback:
+  future.then((socket) { // start of a one-argument closure
+    // The socket is now available.
+    print("Connected");
+    socket.destroy();  // Shuts down the socket in both directions.
+  });
+  print("Callback has been registered, but hasn't been called yet");
 }
